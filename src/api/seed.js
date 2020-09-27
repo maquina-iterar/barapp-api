458899f0839c711faa03ad0ab695ac0049243d88
Christian Escalante <chescalante.ar@gmail.com>
2020-09-27 14:10:09 -0300

    se agregaron 2 bares nuevos

--- src/api/seed.js
+++ src/api/seed.js
@@ -19,6 +19,7 @@ const redesSociales = {
 
 const paises = {
   Argentina: "Argentina",
+  Espana: "España",
 };
 
 const baresSeed = [
@@ -28,12 +29,12 @@ const baresSeed = [
     noMeGusta: 0,
     nombre: "Club de la birra",
     descripcion: "Tiene unas papas con salsas increíbles",
-    fotoUrl: `${process.env.STATIC_FILES_URL}/344B61A5-43D2-4A2F-8248-3C164EF5F156%20-%20Camila%20Grizio.jpeg`,
-    direccion: "Av. Pedro Goyena 60, C1424 CABA",
+    fotoUrl: `${process.env.STATIC_FILES_URL}/club-de-la-birra-argentina-0000.jpeg`,
+    direccion: "Av. Pedro Goyena 60",
     pais: paises.Argentina,
     ubicacionUrl: "https://goo.gl/maps/fsdAcFhHQL4XQRGk7",
     galeria: [
-      `${process.env.STATIC_FILES_URL}/344B61A5-43D2-4A2F-8248-3C164EF5F156%20-%20Camila%20Grizio.jpeg`,
+      `${process.env.STATIC_FILES_URL}/club-de-la-birra-argentina-0000.jpeg`,
     ],
     contactos: [
       {
@@ -42,7 +43,7 @@ const baresSeed = [
       },
     ],
     caracteristicas: {
-      opcionVegetariana: true, // false
+      opcionVegetariana: true,
       musiscaEnVivo: false,
       espacioAlAireLibre: espacioAlAireLibreOptions.Abierto,
     },
@@ -57,13 +58,11 @@ const baresSeed = [
     nombre: "However Bar",
     descripcion:
       "Con cervezas artesanales y mucha buena onda, es una muy buena opción para juntarte con amigos a tomar algo. Además tiene muy buenos precios. ",
-    fotoUrl: `${process.env.STATIC_FILES_URL}/Captura de Pantalla 2020-09-06 a la(s) 14.58.01 - Christian Escalante.png`,
+    fotoUrl: `${process.env.STATIC_FILES_URL}/however-bar-argentina-0000.png`,
     direccion: "Raúl Scalabrini Ortíz 601",
     pais: paises.Argentina,
     ubicacionUrl: "https://goo.gl/maps/1tcWUtctiSdpmfCR9",
-    galeria: [
-      `${process.env.STATIC_FILES_URL}/Captura de Pantalla 2020-09-06 a la(s) 14.58.01 - Christian Escalante.png`,
-    ],
+    galeria: [`${process.env.STATIC_FILES_URL}/however-bar-argentina-0000.png`],
     contactos: [
       {
         redSocial: redesSociales.instagram,
@@ -71,9 +70,69 @@ const baresSeed = [
       },
     ],
     caracteristicas: {
-      opcionVegetariana: true, // false
+      opcionVegetariana: true,
       musiscaEnVivo: false,
       espacioAlAireLibre: espacioAlAireLibreOptions.Abierto,
+    },
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    deletedAt: null,
+  },
+  {
+    slug: slug(`Avant Garden ${paises.Argentina}`),
+    meGusta: 0,
+    noMeGusta: 0,
+    nombre: "Avant Garden",
+    descripcion:
+      "El lugar es hermoso, la carta de cervezas y tragos es variada, y algunas noches podes encontrarte con djs pasando música house en un ambiente súper relajado. ",
+    fotoUrl: `${process.env.STATIC_FILES_URL}/avant-garten-argentina-0000.jpg`,
+    direccion: "Av. del Libertador 3883",
+    pais: paises.Argentina,
+    ubicacionUrl: "https://goo.gl/maps/Xnt97AkHjGGvp5tLA",
+    galeria: [
+      `${process.env.STATIC_FILES_URL}/avant-garten-argentina-0000.jpg`,
+    ],
+    contactos: [
+      {
+        redSocial: redesSociales.instagram,
+        link: "https://instagram.com/avantgartenba",
+      },
+    ],
+    caracteristicas: {
+      opcionVegetariana: true,
+      musiscaEnVivo: true,
+      espacioAlAireLibre: espacioAlAireLibreOptions.Techado,
+    },
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    deletedAt: null,
+  },
+  {
+    slug: slug(`El Tigre del Norte ${paises.Espana}`),
+    meGusta: 0,
+    noMeGusta: 0,
+    nombre: "El Tigre del Norte",
+    descripcion:
+      "Combo de Caña + Cena por muy pocos euros, abudante y buen ambiente.",
+    fotoUrl: `${process.env.STATIC_FILES_URL}/el-tigre-del-norte-espana-0000.jpg`,
+    direccion: "Calle de Hortaleza 23",
+    pais: paises.Espana,
+    ubicacionUrl: "https://goo.gl/maps/CnWhRN8Z17TBmX3Z9",
+    galeria: [
+      `${process.env.STATIC_FILES_URL}/el-tigre-del-norte-espana-0000.jpg`,
+      `${process.env.STATIC_FILES_URL}/el-tigre-del-norte-espana-0001.jpg`,
+    ],
+    contactos: [
+      {
+        redSocial: redesSociales.website,
+        link:
+          "https://www.tripadvisor.com.ar/Restaurant_Review-g187514-d3752906-Reviews-El_Tigre_del_Norte-Madrid.html",
+      },
+    ],
+    caracteristicas: {
+      opcionVegetariana: false,
+      musiscaEnVivo: false,
+      espacioAlAireLibre: espacioAlAireLibreOptions.No,
     },
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
