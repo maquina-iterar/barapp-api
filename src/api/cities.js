53ea9588afae47daff5b522972630839f9122bc2
Christian Escalante <chescalante.ar@gmail.com>
2020-11-01 20:29:23 -0300

    12va iteración

--- src/api/cities.js
+++ src/api/cities.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const nearbyCities = require("nearby-cities");
+const cities = require("all-the-cities");
+const Fuse = require("fuse.js");
 
 const router = express.Router();
+
+router.get("/", async (request, response, next) => {
+  try {
+    const { search } = request.query;
+
+    const options = {
+      keys: ["name", "country"],
+    };
+
+    const fuse = new Fuse(cities, options);
+    const result = fuse.search(search);
+
+    response.json(result.slice(0, 100).map((x) => x.item));
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/location", async (request, response, next) => {
   try {
